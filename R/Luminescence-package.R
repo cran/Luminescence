21e13c6449d6b9f7e1e2a53705f0d38f7e029e61
Sebastian Kreutzer <sebastian.kreutzer@aber.ac.uk>
2020-11-29 05:00:03 +0000

    version 0.9.9

--- R/Luminescence-package.R
+++ R/Luminescence-package.R
@@ -60,7 +60,7 @@
 #'
 #' **Project website**
 #'
-#' - [https://www.r-luminescence.org]()
+#' - [https://r-luminescence.org]()
 #'
 #' **Project source code repository**
 #'
@@ -1219,7 +1219,7 @@ NULL
 #'
 #' @references Kreutzer, S., Martin, L., Guérin, G., Tribolo, C., Selva, P., Mercier, N., 2018.
 #' Environmental Dose Rate Determination Using a Passive Dosimeter: Techniques and Workflow for alpha-Al2O3:C Chips.
-#' Geochronometria 45, 56–67. \doi{10.1515/geochr-2015-0086}
+#' Geochronometria 45, 56–67.
 #'
 #' @keywords datasets
 #'
